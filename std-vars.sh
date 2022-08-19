4b51b35270da14dc204414e0ee1eee28fb469a85
Laurent Nominé <laurent.nomine@gmail.com>
2022-08-19 09:02:46 +0200

    Add gateway hack

--- std-vars.sh
+++ std-vars.sh
@@ -2,16 +2,25 @@
 
 apt update ; apt install -y ipcalc
 
+interface=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)')
 link=$(ip -4 -o addr show up primary scope global | while read -r num dev fam addr rest; do echo ${addr}; done)
 ip=$(echo ${link} | cut -d '/' -f1)
 netmask=$(ipcalc ${link} | grep Netmask | awk '{ print $2 }')
 gateway=$(ip route show default | awk '/default/ { print $3 }')
 dns=$(grep nameserver /etc/resolv.conf | grep -v "\#" | head -1 | awk '{ print $2 }')
+mirror="deb.debian.org"
 password=$(cat /tmp/password)
+earlycheck="exit 0"
 
 if [ "$dns" == "127.0.0.53" ]; 
 then
 dns=$(grep -w "DNS" /etc/systemd/resolved.conf | grep -v "\#" | cut -d '=' -f2 | awk '{ print $1 }')
+fi
+
+if [ "$gateway" == "10.255.255.1" ]; 
+then
+mirror="212.27.32.66"
+earlycheck="sh -c 'ip link set dev $interface up ; ip addr add $link dev $interface ; ip route add $gateway dev $interface; ip route add default via $gateway dev $interface; mv /sbin/ip /sbin/ip2 ; echo "exit 0" > /sbin/ip'"
 fi
 
 grep -q "/boot" /boot/grub/grub.cfg
