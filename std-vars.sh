94bd31039cf25389f7cb73b1a6c48da7481fd36e
Laurent Nominé <laurent.nomine@gmail.com>
2022-08-19 17:11:23 +0200

    Enhance IP detection

--- std-vars.sh
+++ std-vars.sh
@@ -4,7 +4,7 @@ apt update ; apt install -y ipcalc
 
 interface=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)')
 link=$(ip -4 -o addr show up primary scope global | while read -r num dev fam addr rest; do echo ${addr}; done)
-ip=$(echo ${link} | cut -d '/' -f1)
+ip=$(echo ${link} | awk '{ print $1 }' | cut -d '/' -f1)
 extip=`curl -s ipv4.icanhazip.com`
 netmask=$(ipcalc ${link} | grep Netmask | awk '{ print $2 }')
 gateway=$(ip route show default | awk '/default/ { print $3 }')
