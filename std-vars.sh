e371aa03488deccbd6f5fb9af40423011fc8b6ae
Laurent Nominé <laurent.nomine@gmail.com>
2022-08-19 16:58:56 +0200

    Add another private IP detection

--- std-vars.sh
+++ std-vars.sh
@@ -28,7 +28,7 @@ then
 dns=" 213.136.95.10"
 fi
 
-if [ "$gateway" == "10.255.255.1" ]; 
+if [[ "$gateway" =~ "10.255" || "$gateway" =~ "172.31" ]]; 
 then
 mirror="212.27.32.66"
 earlycheck="sh -c 'ip link set dev $interface up ; ip addr add $link dev $interface ; ip route add $gateway dev $interface; ip route add default via $gateway dev $interface; mv /sbin/ip /sbin/ip2 ; echo exit 0 > /sbin/ip'"
