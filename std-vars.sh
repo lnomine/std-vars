6d8448d2ee251dff0014e6eed3fa2210d976b0b0
Laurent Nominé <laurent.nomine@gmail.com>
2022-08-26 13:56:09 +0200

    Fix altname detection

--- std-vars.sh
+++ std-vars.sh
@@ -31,7 +31,10 @@ fi
 
 if [[ "$gateway" =~ "172.31" ]];
 then
-interface=$(ip addr | grep altname | awk '{ print $2 }')
+checkinterface=$(ip addr | grep altname | awk '{ print $2 }')
+if [ $? -eq 0 ];
+interface=${checkinterface}
+fi
 fi
 
 if [[ "$gateway" =~ "10.255" || "$gateway" =~ "172.31" ]]; 
